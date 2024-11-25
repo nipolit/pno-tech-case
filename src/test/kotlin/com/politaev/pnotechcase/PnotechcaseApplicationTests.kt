0060e4bb9ae47e67bea34fedc76be179c8a3ce61
Nikita Politaev <nipolit@gmail.com>
2024-11-25 20:46:21 +0100

    Verify module structure and generate C4 diagrams

--- src/test/kotlin/com/politaev/pnotechcase/PnotechcaseApplicationTests.kt
+++ src/test/kotlin/com/politaev/pnotechcase/PnotechcaseApplicationTests.kt
@@ -1,13 +1,27 @@
 package com.politaev.pnotechcase
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 
 @SpringBootTest
 class PnotechcaseApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
+
+    @Test
+    fun verifyModularStructure() {
+        val modules = ApplicationModules.of(PnotechcaseApplication::class.java)
+        assertDoesNotThrow { modules.verify() }
+        Documenter(modules)
+            .writeDocumentation()
+            .writeModuleCanvases()
+            .writeModulesAsPlantUml()
+            .writeIndividualModulesAsPlantUml()
+    }
 
 }
